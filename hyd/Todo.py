8ee982a4e011a78ac130bd1e7c2f6e549db114b8
Futuregus <152952979+Futuregus@users.noreply.github.com>
2026-04-24 21:28:27 -0400

    Delete hyd/Todo.py

--- hyd/Todo.py
+++
@@ -1,5 +0,0 @@
-# TODO: Everything:D
-# TODO: Refactor the entire codebase to follow my hȳd — Code Style Guide
-# TODO: I need to make a website for hȳd i need a free way to make it and host it.
-# TODO: Kidnap  my brother and force him to make a website for hȳd (ok maybe not kidnap but ask my brother nicely-ish mostly cuz i can be rude to my brother but i still love him and i would never actually kidnap him maybe.....)
-# TODO: Finnish hȳd so i can impress a girl and get her to date me cause i have no girlfriend and I'm so lonely :( (ok maybe i'm not lonely but it would be nice to have a girlfriend and i think having a cool app like hȳd would help me get a girlfriend)
